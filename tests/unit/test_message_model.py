16eb342db2f84723c6eaa0a1785e864993b89441
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-05-29 08:48:38 +0300

    tests(unit): add more test coverage for deserialize function

--- tests/unit/test_message_model.py
+++ tests/unit/test_message_model.py
@@ -5,5 +5,25 @@ def test_deserialize_should_work_with_valid_payload() -> None:
     payload = b'{"data": 123}'
     payload_deser = deserialize(payload)
 
-    assert type(payload_deser) != bytes
     assert payload_deser == {"data": 123}
+
+
+def test_deserialize_should_return_none_if_payload_is_not_valid_utf8() -> None:
+    payload = b'{"data": \xa0\xa1}'
+    payload_deser = deserialize(payload)
+
+    assert payload_deser is None
+
+
+def test_deserialize_should_return_none_if_deserialization_fails() -> None:
+    payload = b'\\'
+    payload_deser = deserialize(payload)
+
+    assert payload_deser is None
+
+
+def test_deserialize_should_return_empty_if_payload_is_empty() -> None:
+    payload = b'{}'
+    payload_deser = deserialize(payload)
+
+    assert payload_deser == {}
